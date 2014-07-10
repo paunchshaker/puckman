7ae16e227d612df08078b2e6ff09e2e9279c3711
Dave Larson <ernfrid@gmail.com>
2014-07-09 23:42:07 -0500

    Fix some formatting and make sure to call the super class constructor

--- puckman/player.py
+++ puckman/player.py
@@ -5,9 +5,8 @@ from puckman.person import Person
 class Player(Person):
     """The Player class defines general info about hockey players."""
 
-    def __init__(self, first_name, last_name, position, team = None):
+    def __init__(self, first_name, last_name, position, team=None):
         """Initialize a new Player"""
-        self.first_name = first_name
-        self.last_name = last_name
+        super().__init__(first_name, last_name)
         self.team = team
         self.position = position
