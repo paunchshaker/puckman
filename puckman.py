be7c73c25f0824877677f8a34e94fbc411e7b756
Dave Larson <ernfrid@gmail.com>
2014-07-11 23:39:46 -0500

    Implement flask webserver as UI. Needs a template

--- puckman.py
+++ puckman.py
@@ -1,38 +1,42 @@
 #!/usr/bin/env python
 
 import random
+from flask import Flask, redirect, url_for
 from puckman.league import League
 from puckman.team import Team
 from puckman.record import Record
 from puckman.game import Game
 
-def main():
-    """the game"""
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    """the main screen for the game"""
+    return str(app.league)
+
+def create_test_league():
+    """This method creates a junk league for demo purposes"""
     team1 = Team(name = "Komets", city = "Fort Wayne", skill = 90, record = Record(), abbreviation = "FTW")
     team2 = Team(name = "Ice", city = "Indianapolis", skill = 80, record = Record(), abbreviation = "IND")
     team3 = Team(name = "Aeros", city = "Houston", skill = 70, record = Record(), abbreviation = "HOU")
     team4 = Team(name = "Whoopie", city = "Macon", skill = 75, record = Record(), abbreviation = "MAC")
 
-    league = League(name="Fake League", teams=[team1, team2, team3, team4])
-    while 1:
-        command = input("Press enter for next season, type p to print standings, or type q to end: ")
-        command = command.strip().lower()
-        if command == "p":
-            print(str(league))
-        elif command == "q":
-            break
-        else:
-            i = 0
-            for team in league.teams:
-                team.record = Record()
-            while i < 82:
-                matchups = league.teams
-                random.shuffle(matchups)
-                for home, visitor in [(matchups[0], matchups[1]), (matchups[2], matchups[3])]:
-                    game = Game(home = home, visitor = visitor)
-                    game.play()
-                i += 1
+    return League(name="Fake League", teams=[team1, team2, team3, team4])
+
+@app.route('/action/sim_season')
+def sim_season():
+    i = 0
+    for team in app.league.teams:
+        team.record = Record()
+    while i < 82:
+        matchups = app.league.teams
+        random.shuffle(matchups)
+        for home, visitor in [(matchups[0], matchups[1]), (matchups[2], matchups[3])]:
+            game = Game(home = home, visitor = visitor)
+            game.play()
+        i += 1
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    main()
+    app.league = create_test_league()
+    app.run(debug = True)
