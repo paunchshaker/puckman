25160d4fc5b540f6c4efc6b3c54b80fa62d3dc28
paunchshaker <ernfrid@gmail.com>
2014-10-10 23:01:47 -0500

    basic player generator

---
+++ puckman/player_generator.py
@@ -0,0 +1,18 @@
+"""This module generates players"""
+
+from puckman.player import Player
+from puckman.name_generator import NameGenerator
+from puckman.position_generator import PositionGenerator
+
+class PlayerGenerator:
+    """Generates players"""
+
+    def __init__(self):
+        """Initialize the generator"""
+        self.name_generator = NameGenerator()
+        self.position_generator = PositionGenerator()
+
+    def generate(self):
+        name = self.name_generator.generate()
+        position = self.position_generator.generate()
+        return Player(name, position)
