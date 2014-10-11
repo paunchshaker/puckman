b8deddeea3be752c58608fb192ade9899d9cf3b5
paunchshaker <ernfrid@gmail.com>
2014-10-10 21:53:36 -0500

    adopt name class

--- test/roster_test.py
+++ test/roster_test.py
@@ -4,12 +4,13 @@
 from unittest import TestCase
 from puckman.roster import Roster
 from puckman.player import Player
+from puckman.name import Name
 
 class TestRoster(TestCase):
     def setUp(self):
         self.roster = Roster()
-        self.player1 = Player(first_name = "Trevor", last_name = "Linden", team = None, position = None)
-        self.player2 = Player(first_name = "Scott", last_name = "Pilgrim", team = None, position = None)
+        self.player1 = Player(Name(forename = "Trevor", surname = "Linden"), team = None, position = None)
+        self.player2 = Player(Name(forename = "Scott", surname = "Pilgrim"), team = None, position = None)
 
     def test_creation(self):
         self.assertEqual(list(), self.roster.players())
