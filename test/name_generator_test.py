935be0ce6d95534aa7f2122e80c91766f6bff3b4
paunchshaker <ernfrid@gmail.com>
2014-12-24 23:48:56 -0600

    fix some sloppiness

--- test/name_generator_test.py
+++ test/name_generator_test.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-"""Unit tests for the Player class"""
+"""Unit tests for the NameGenerator class"""
 
 from unittest import TestCase
 from mock import Mock
 from puckman.name_generator import NameGenerator
 
-class TestPlayer(TestCase):
+class TestNameGenerator(TestCase):
     def test_creation(self):
         generator = NameGenerator()
         self.assertIsNotNone(generator)
