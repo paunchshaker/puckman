71943f0cdb6a256f9e95bd96a04bc47913ca282c
paunchshaker <ernfrid@gmail.com>
2016-11-15 21:43:55 -0600

    Change PMDataObject to use peewee

--- test/data_object_test.py
+++ test/data_object_test.py
@@ -3,12 +3,14 @@
 
 from unittest import TestCase
 from puckman.data_object import PMDataObject
+from puckman.data_object import db
 
 class TestPMDataObject(TestCase):
     def setUp(self):
+        db.init(':memory:')
+        db.create_tables([ PMDataObject ])
         self.data_object = PMDataObject()
+        self.data_object.save()
 
     def test_creation(self):
-        self.assertIsNotNone(self.data_object.uuid)
-    def test_id(self):
-        self.assertEqual(self.data_object.uuid.hex, self.data_object.id())
+        self.assertIsNotNone(self.data_object.id)
