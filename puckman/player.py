379b037288cd634acb3def32180e66ab321013d4
paunchshaker <ernfrid@gmail.com>
2016-11-16 21:42:52 -0600

    move towards ORM-backed players, but currently broken

--- puckman/player.py
+++ puckman/player.py
@@ -1,10 +1,15 @@
 """This module contains code describing a player"""
 
 from puckman.person import Person
+from puckman.team import Team
+
+from peewee import *
 
 class Player(Person):
 
     """The Player class defines general info about hockey players."""
+    position = FixedCharField(max_length=1)
+    team = ForeignKeyField(Team, related_name='roster')
 
     def __init__(self, name, position, team=None):
         """Initialize a new Player"""
--- test/player_test.py
+++ test/player_test.py
@@ -3,14 +3,17 @@
 
 from unittest import TestCase
 from mock import Mock
+from puckman.data_object import db, PMDataObject
 from puckman.team import Team
 from puckman.player import Player
 from puckman.name import Name
 
 class TestPlayer(TestCase):
     def setUp(self):
-        self.team = Team(name = "Canucks", city = "Vancouver", skill = 100, record = None, abbreviation = "VAN")
-        self.player = Player(Name(forename = "Cliff", surname = "Ronning"), team = self.team, position = "C")
+        db.init(':memory:')
+        db.create_tables(PMDataObject.__subclasses__())
+        self.team = Team.create(name = "Canucks", city = "Vancouver", skill = 100, record = None, abbreviation = "VAN")
+        self.player = Player.create(name=Name(forename = "Cliff", surname = "Ronning"), team = self.team, position = "C")
 
     def test_creation(self):
         self.assertEqual(self.player.name, Name(forename = "Cliff", surname = "Ronning"))
