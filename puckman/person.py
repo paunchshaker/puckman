236b33b9ccee69802a9a4154e8448fe61d2ebbbf
paunchshaker <ernfrid@gmail.com>
2016-11-16 20:39:47 -0600

    make person database compatible

--- puckman/person.py
+++ puckman/person.py
@@ -1,13 +1,20 @@
 """This module contains code describing a person in the most basic sense"""
+from peewee import *
 from puckman.data_object import PMDataObject
 
 class Person(PMDataObject):
 
     """The Person class defines general info about all people."""
+    forename = TextField()
+    surname = TextField()
 
     def __init__(self, name):
         """Initialize a new Person"""
-        super().__init__()
+        super().__init__(forename=name.forename, 
+                surname=name.surname)
+        # NOTE Storing name here for historical reasons
+        # May want to put in accessors to manage database updates through name
+        # or make it read only, but for now leaving alone
         self.name = name
 
     def full_name(self):
