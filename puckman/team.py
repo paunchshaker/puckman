798ca08f897c605495d7313780ce17ec84d1b02f
paunchshaker <ernfrid@gmail.com>
2016-11-17 22:47:24 -0600

    add in TeamStats class

--- puckman/team.py
+++ puckman/team.py
@@ -6,6 +6,38 @@ from puckman.roster import Roster
 from collections import deque
 from peewee import *
 
+class TeamStats(PMDataObject):
+    """Stats for a Team"""
+    wins = IntegerField(default=0)
+    losses = IntegerField(default=0)
+    ties = IntegerField(default=0)
+
+    goals_for = IntegerField(default=0)
+    goals_against = IntegerField(default=0)
+
+    def add_win(self):
+        """Add a win to the record."""
+        self.wins += 1
+        self.save()
+
+    def add_loss(self):
+        """Add a loss to the record."""
+        self.losses += 1
+        self.save()
+
+    def add_tie(self):
+        """Add a tie to the record."""
+        self.ties += 1
+        self.save()
+
+    def add_goals(self, goals):
+        self.goals += goals
+        self.save()
+
+    def add_goals_against(self, goals):
+        self.goals_against += goals
+        self.save()
+
 class Team(PMDataObject):
 
     """The Team class defines information about a hockey team."""
@@ -14,9 +46,6 @@ class Team(PMDataObject):
     skill = FloatField(null=False)
     abbreviation = CharField(max_length=3)
 
-    # Stats
-    goals_for = IntegerField(default=0)
-    goals_against = IntegerField(default=0)
     # Note Roster will move to a foreign key constraint on Player
     #self.roster = Roster()
     # Note Record will move to a foreign key constraint on Player as well.
