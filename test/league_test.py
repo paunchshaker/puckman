6341d6c66f271cd50ea6987602c9f9563be389fb
paunchshaker <ernfrid@gmail.com>
2016-11-19 10:48:32 -0600

    add test of teams method

--- test/league_test.py
+++ test/league_test.py
@@ -6,18 +6,28 @@ from mock import Mock
 from puckman.data_object import db
 from puckman.league import League
 from puckman.season import Season
+from puckman.team import Team
 
 class TestLeague(TestCase):
     def setUp(self):
         db.init(':memory:')
-        db.create_tables([League, Season])
+        db.create_tables([Team, League, Season])
 
         self.league = League.create(name="Band Battle")
         self.season = Season.create(league=self.league, start_year=2016, end_year=2017)
         self.league.current_season = self.season.id
         self.league.save()
+        self.team = Team.create(name="Sex Bob-omb",
+                city="Toronto",
+                skill=90,
+                abbreviation="TOR",
+                league=self.league)
     
     def test_creation(self):
         self.assertEqual(self.league.name, "Band Battle")
         self.assertIsNotNone(self.league.id)
         self.assertEqual(self.league.current_season, self.season.id)
+
+    def test_teams(self):
+        self.assertIsNotNone(self.league.teams)
+        self.assertEqual(self.league.teams[0].name, "Sex Bob-omb")
