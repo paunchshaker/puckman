36e90932d67cf22d0411a42d25f8161d750c641d
paunchshaker <ernfrid@gmail.com>
2016-11-18 22:01:53 -0600

    make league, orm-backed and have a season

--- puckman/league.py
+++ puckman/league.py
@@ -1,28 +1,9 @@
 """This module contains code describing a league"""
-from tabulate import tabulate
 from puckman.data_object import PMDataObject
-from puckman.roster import Roster
+from peewee import *
 
 class League(PMDataObject):
-
     """The League class defined information about a group of teams"""
-
-    def __init__(self, name, teams):
-        """Initialize a new League"""
-        super().__init__()
-        self.name = name
-        self.teams = teams
-        self.roster = Roster()
-
-    def __str__(self):
-        standings = [[
-            "{0} {1}".format(x.city, x.name),
-            x.record.wins,
-            x.record.losses,
-            x.record.ties,
-            x.record.wins * 2 + x.record.ties] for x in
-            sorted(self.teams,
-                key=lambda team: team.record.wins * 2 + team.record.ties,
-                reverse=True)]
-        return tabulate(standings, headers=["Team", "Wins", "Losses", "Ties", "Points"])
-
+    name = TextField()
+    # NOTE Using IntegerField to store the id of the current season
+    current_season = IntegerField(null=True)
--- test/league_test.py
+++ test/league_test.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
-"""Unit tests for the Team class"""
+"""Unit tests for the League and Season class"""
 
 from unittest import TestCase
 from mock import Mock
-from puckman.record import Record
-from puckman.team import Team
+from puckman.data_object import db
 from puckman.league import League
+from puckman.season import Season
 
 class TestLeague(TestCase):
     def setUp(self):
-        self.team1 = Team(name = "Sex Bob-omb", city = "Toronto", skill = 90, record = Record(1,0), abbreviation = "SBO")
-        self.team2 = Team(name = "Crash and the Boys", city = "Toronto", skill = 80, record = Record(0,1), abbreviation = "CAB")
-        self.league = League(name="Band Battle", teams=[self.team1, self.team2])
+        db.init(':memory:')
+        db.create_tables([League, Season])
+
+        self.league = League.create(name="Band Battle")
+        self.season = Season.create(league=self.league, start_year=2016, end_year=2017)
+        self.league.current_season = self.season.id
+        self.league.save()
     
     def test_creation(self):
         self.assertEqual(self.league.name, "Band Battle")
-        self.assertListEqual(self.league.teams, [self.team1, self.team2])
-        self.assertIsNotNone(self.league.id())
-
-    def test_str(self):
-        self.assertEqual(str(self.league), 
-        "Team                          Wins    Losses    Ties    Points\n--------------------------  ------  --------  ------  --------\nToronto Sex Bob-omb              1         0       0         2\nToronto Crash and the Boys       0         1       0         0")
-
+        self.assertIsNotNone(self.league.id)
+        self.assertEqual(self.league.current_season, self.season.id)
