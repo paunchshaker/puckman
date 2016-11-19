be161c3550b0d1fdddfe7df59c6a107db0031d6d
paunchshaker <ernfrid@gmail.com>
2016-11-18 22:17:02 -0600

    beginnings of orm-backed team stats

--- puckman/team.py
+++ puckman/team.py
@@ -1,73 +1,46 @@
 """This module contains code describing a hockey team"""
 
 from puckman.data_object import PMDataObject
-from puckman.roster import Roster
+from puckman.league import League
 
 from collections import deque
 from peewee import *
 
-class TeamStats(PMDataObject):
-    """Stats for a Team"""
-    wins = IntegerField(default=0)
-    losses = IntegerField(default=0)
-    ties = IntegerField(default=0)
-
-    goals_for = IntegerField(default=0)
-    goals_against = IntegerField(default=0)
-
-    def add_win(self):
-        """Add a win to the record."""
-        self.wins += 1
-        self.save()
-
-    def add_loss(self):
-        """Add a loss to the record."""
-        self.losses += 1
-        self.save()
-
-    def add_tie(self):
-        """Add a tie to the record."""
-        self.ties += 1
-        self.save()
-
-    def add_goals(self, goals):
-        self.goals += goals
-        self.save()
-
-    def add_goals_against(self, goals):
-        self.goals_against += goals
-        self.save()
-
 class Team(PMDataObject):
 
     """The Team class defines information about a hockey team."""
+    league = ForeignKeyField(League, related_name='teams')
+
     name = TextField(null=False)
     city = TextField(null=False)
     skill = FloatField(null=False)
     abbreviation = CharField(max_length=3)
 
-    # Note Roster will move to a foreign key constraint on Player
-    #self.roster = Roster()
-    # Note Record will move to a foreign key constraint on Player as well.
-
     class Meta:
         constraints = [Check('length(abbreviation) = 3')]
 
+    def current_season_stats(self):
+        for stats in self.stats:
+            return stats.id == self.league.current_season
+
     def won(self):
         """Team has won a game"""
-        self.record.add_win()
+        self.current_season_stats().add_win()
+        self.current_season_stats().save()
 
     def lost(self):
         """Team has lost a game"""
-        self.record.add_loss()
+        self.current_season_stats().add_loss()
+        self.current_season_stats().save()
 
     def tied(self):
         """Team tied a game"""
-        self.record.add_tie()
+        self.current_season_stats().add_tie()
+        self.current_season_stats().save()
 
     def register_result(self, goals_for, goals_against):
         """Register the result of a game"""
-        self.goals_for += goals_for
-        self.goals_against += goals_against
-        self.save()
+        self.current_season_stats().goals_for += goals_for
+        self.current_season_stats().goals_against += goals_against
+        self.current_season_stats().save()
 
--- test/team_test.py
+++ test/team_test.py
@@ -6,44 +6,46 @@ from unittest import TestCase
 from mock import Mock
 from puckman.data_object import db
 from puckman.team import Team
-from puckman.record import Record
+from puckman.league import League
 
 class TestTeam(TestCase):
     def setUp(self):
         db.init(':memory:')
-        db.create_tables([Team])
-        self.team = Team.create(name = "Sex Bob-omb", city = "Toronto", skill = 90, record = Record(), abbreviation = "TOR")
-        self.team.record
-        #self.team.save()
+        db.create_tables([Team, League])
+        
+        self.league = League.create(name="Band Battle")
+
+        self.team = Team.create(name="Sex Bob-omb", 
+                city="Toronto", 
+                skill=90,
+                abbreviation="TOR", 
+                league=self.league)
     
     def test_creation(self):
+        self.assertEqual(self.team.league, self.league)
         self.assertEqual(self.team.name, "Sex Bob-omb")
         self.assertEqual(self.team.city, "Toronto")
         self.assertEqual(self.team.skill, 90)
-        self.assertEqual(self.team.record, Record())
         self.assertEqual(self.team.abbreviation, "TOR")
-        self.assertEqual(self.team.goals_for, 0)
-        self.assertEqual(self.team.goals_against, 0)
         self.assertIsNotNone(self.team.id)
 
     def test_bad_abbreviation(self):
         with self.assertRaises(IntegrityError):
-            Team.create(name = "The Clash at Demonhead", city = "New York", skill = 50, record = Record(), abbreviation = "T")
-
+            Team.create(name = "The Clash at Demonhead", city = "New York", skill = 50, abbreviation = "T")
     
-    def test_win(self):
-        self.team.won()
-        self.assertEqual(self.team.record, Record(1,0))
+    #def test_win(self):
+    #    self.team.won()
+    #    self.assertEqual(self.team.record, Record(1,0))
 
-    def test_loss(self):
-        self.team.lost()
-        self.assertEqual(self.team.record, Record(0,1))
+    #def test_loss(self):
+    #    self.team.lost()
+    #    self.assertEqual(self.team.record, Record(0,1))
 
-    def test_tied(self):
-        self.team.tied()
-        self.assertEqual(self.team.record, Record(0,0,1))
+    #def test_tied(self):
+    #    self.team.tied()
+    #    self.assertEqual(self.team.current_season_stats()., Record(0,0,1))
 
-    def test_register_results(self):
-        self.team.register_result(5, 2)
-        self.assertEqual(self.team.goals_for, 5)
-        self.assertEqual(self.team.goals_against, 2)
+    #def test_register_results(self):
+    #    self.team.register_result(5, 2)
+    #    self.assertEqual(self.team.goals_for, 5)
+    #    self.assertEqual(self.team.goals_against, 2)
