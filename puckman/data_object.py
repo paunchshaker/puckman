ff13b59f11d150877b137d130478de48de5f1b8f
paunchshaker <ernfrid@gmail.com>
2016-11-21 21:57:44 -0600

    clean up data_object

--- puckman/data_object.py
+++ puckman/data_object.py
@@ -5,10 +5,17 @@ from peewee import *
 # Declare our intention to manage the db at runtime
 db = SqliteDatabase(None)
 
-
 class PMDataObject(Model):
-    deferred_relations = dict()
 
     """The PMDataObject provides basic information for all data objects"""
+
+    # NOTE This dict is intended to store references to classes that can't be
+    # imported. It will need to be initialized with class objects outside of
+    # this module.
+    deferred_relations = dict()
+
     class Meta:
+
+        """This defines Meta-information for the peewee ORM"""
+
         database = db
