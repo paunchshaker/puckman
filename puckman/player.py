5d4d817567efb94171fcaee832f8c89c9ffd9bf9
paunchshaker <ernfrid@gmail.com>
2016-11-21 21:59:51 -0600

    clean up player class

--- puckman/player.py
+++ puckman/player.py
@@ -4,11 +4,12 @@ from puckman.data_object import PMDataObject
 from puckman.person import Person
 from puckman.team import Team
 
-from peewee import *
+from peewee import FixedCharField, ForeignKeyField
 
 class Player(PMDataObject):
 
     """The Player class defines general info about hockey players."""
+
     position = FixedCharField(max_length=1, null=False)
     person = ForeignKeyField(Person, related_name='player_role', null=False)
     team = ForeignKeyField(Team, related_name='roster', null=True)
