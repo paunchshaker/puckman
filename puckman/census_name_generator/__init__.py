1f9ce821512a48661b958628b27a5880421291a5
paunchshaker <ernfrid@gmail.com>
2016-12-24 23:41:13 -0600

    add name generator based on census names and frequencies

---
+++ puckman/census_name_generator/__init__.py
@@ -0,0 +1,5 @@
+import os.path
+
+data_dir = os.path.join(os.path.dirname(__file__), 'data')
+surname_source = os.path.join(data_dir, 'surnames.csv')
+forename_source = os.path.join(data_dir, 'male_first.csv')
