75153f6e9874ce06646d2a78cbd78bc43b729124
paunchshaker <ernfrid@gmail.com>
2016-12-27 13:11:34 -0600

    fields for skill

--- puckman/player.py
+++ puckman/player.py
@@ -5,7 +5,7 @@ from puckman.person import Person
 from puckman.season import Season
 from puckman.team import Team
 
-from peewee import FixedCharField, ForeignKeyField
+from peewee import FixedCharField, ForeignKeyField, FloatField
 
 class Player(PMDataObject):
 
@@ -14,6 +14,18 @@ class Player(PMDataObject):
     position = FixedCharField(max_length=1, null=False)
     person = ForeignKeyField(Person, related_name='player_role', null=False)
     team = ForeignKeyField(Team, related_name='roster', null=True)
+
+    # skater skill levels
+    # rate of scoring per 60 minutes
+    scoring_rate = FloatField(null=False)
+    # shot rate per 60 minutes
+    shot_rate = FloatField(null=False)
+    # shot suppression rate in shots reduced per 60 minutes
+    shot_suppression = FloatField(null=False)
+
+    # scoring suppression. Goalies only for now. May want to split into two
+    # ratings, one for skaters and one for goalies
+    scoring_suppression = FloatField(null=False)
 
     def current_team_season_stats(self):
         """Return the PlayerStats object for the current season and team"""
